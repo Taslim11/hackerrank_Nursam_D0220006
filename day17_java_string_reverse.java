46117f59a396c43618035bda8fedd80efc523e87
Nursam442 <Nursamhalal90@gmail.com>
2022-02-18 06:28:21 +0700

    menambah file baru h-17

---
+++ day17_java_string_reverse.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        boolean isPalindrome = false;
+        StringBuilder sb = new StringBuilder(A);
+        String bk = sb.reverse().toString();
+        System.out.println(A.equals(bk)?"Yes":"No");
+        
+        
+}
+
+}
+
+
+
+
