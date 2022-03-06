8401ba8f76d3bbd17581d0e2d399fc044f7b1bf7
Nursam442 <Nursamhalal90@gmail.com>
2022-03-06 18:40:27 +0700

    menambah file baru h-33

---
+++ day33_java_list.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int ele = sc.nextInt();
+            al.add(ele);
+        }
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+            String query = sc.next();
+            if (query.equals("Insert")) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                al.add(x, y);
+            } else {
+                int x = sc.nextInt();
+                al.remove(x);
+            }
+        }
+        Iterator i = al.iterator();
+        while (i.hasNext()) {
+            System.out.print(i.next() + " ");
+        }
+        sc.close();
+
+    }
+}
