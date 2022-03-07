4d684c50e3736c152cedf6a8a2178591c4d4c4c2
Nursam442 <Nursamhalal90@gmail.com>
2022-03-07 19:20:03 +0700

    menambah file baru h-34

---
+++ day34_java_map.java
@@ -0,0 +1,28 @@
+
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        HashMap<String, Integer> a = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            int phone = in.nextInt();
+            a.put(name, phone);
+            in.nextLine();
+        }
+        while (in.hasNext()) {
+            String s = in.nextLine();
+            if (a.containsKey(s)) {
+                System.out.println(s + "=" + a.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
